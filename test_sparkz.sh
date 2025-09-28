3db822e8c6a851a56becb777fae20d1c9908c074
Guy Durant <guydurant@btinternet.com>
2025-09-28 15:03:16 +0100

    Remove old test scripts

--- test_sparkz.sh
+++
@@ -1,8 +0,0 @@
-sparkz \
---protein_pdb_path /vols/opig/projects/guy-affinity/POCKET_DESIGN/sparkz_analysis/datasets/runs_n_poses/processed_data/7lpw/7lpw_YBA_protein.pdb \
---ligand_sdf_path /vols/opig/projects/guy-affinity/POCKET_DESIGN/sparkz_analysis/datasets/runs_n_poses/processed_data/7lpw/7lpw_YBA_ligand.sdf \
---pid 7lpw \
---ccd YBA \
---num_iterations 10 \
---recycling_steps 0 \
---sampling_steps 25 \
